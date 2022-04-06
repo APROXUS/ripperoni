2b7db87de6887177ca9ff25719193e0443eee345
albie6544 <49698766+albie6544@users.noreply.github.com>
2022-04-06 04:56:40 -0400

    Base UI Elements

---
+++ Ripper/MVVM/ViewModel/DownloadsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ripper.MVVM.ViewModel
+{
+    class DownloadsViewModel
+    {
+
+    }
+}
---
+++ Ripper/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ripper.Core;
+
+namespace Ripper.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
