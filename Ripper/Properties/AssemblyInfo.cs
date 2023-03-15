bc54b4280e75cf7c68debc82d0a1a5a1abfbdc1f
Albie <49698766+albie6544@users.noreply.github.com>
2023-03-14 22:19:59 -0400

    Corrected versioning

--- Ripper/Properties/AssemblyInfo.cs
+++ Ripper/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.3.4.*")]
+[assembly: AssemblyVersion("1.3.5.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
